3d537f7693579be66db5a53cff560773be7a01a0
Xiomara S <xiomisalamanca21@hotmail.com>
2023-11-26 12:21:12 -0500

    creación de la rama proyect-5

--- src/router/list-edit-router.js
+++ src/router/list-edit-router.js
@@ -3,62 +3,67 @@ const router = express.Router();
 const checkerHTTPMethod = require('../middleware/http-method-checker');
 
 module.exports = function (tasks) {
-    const handleErrors = (req, res, next) => {
-        if (req.method === 'POST' && (!req.body || Object.keys(req.body).length === 0)) {
-            return res.status(400).json({ message: 'Empty body for POST request' });
-          }
+  const handleErrors = (req, res, next) => {
+    if (req.method === 'POST' && (!req.body || Object.keys(req.body).length === 0)) {
+      return res.status(400).json({ message: 'The body of the POST request is empty' });
+    }
 
-          if (req.method === 'POST' && (!req.body.id || !req.body.otherAttribute)) {
-            return res.status(400).json({ message: 'Invalid information or missing attributes to create the task' });
-          }
-          if ((req.method === 'PUT' || req.method === 'PATCH') && (!req.body || Object.keys(req.body).length === 0)) {
-            return res.status(400).json({ message: 'Empty body for PUT/PATCH request' });
-          }
-      
-         if ((req.method === 'PUT' || req.method === 'PATCH') && (!req.body.id || !req.body.otherAttribute)) {
-        return res.status(400).json({ message: 'Invalid information or missing attributes to update the task' });
-           }
-           next();
-        };
+    if (req.method === 'POST' && (!req.body.id || !req.body.anotherAttribute)) {
+      return res.status(400).json({ message: 'Invalid information or missing attributes to create the task' });
+    }
 
-        router.use(checkerHTTPMethod);
-        router.use(handleErrors);
+    if ((req.method === 'PUT' || req.method === 'PATCH') && (!req.body || Object.keys(req.body).length === 0)) {
+      return res.status(400).json({ message: 'The body of the PUT/PATCH request is empty' });
+    }
 
-        router.post('/create', (req, res) => {
-            const newTask = { ...req.body };
+    if ((req.method === 'PUT' || req.method === 'PATCH') && (!req.body.id || !req.body.anotherAttribute)) {
+      return res.status(400).json({ message: 'Invalid information or missing attributes to update the task' });
+    }
 
-            if (!newTask.id) {
-                res.status(400).json({ message: 'Task ID is required' });
-                return;
-              }
-              tasks.push(newTask);
-              res.json(newTask);
-  });
+    next(); 
+  };
+  
+  router.use(validateHTTPMethod);
 
-        router.delete('/eliminate/:id', (req, res) => {
-             const taskId = req.params.id;
-            const taskIndex = tasks.findIndex(task => task.id === taskId);
-            if (taskIndex !== -1) {
+  router.use(handleErrors);
+  
+    router.post('/create', (req, res) => {
+    const newTask = { ...req.body };
+    
+    if (!newTask.id || !newTask.description) {
+      res.status(400).json({ message: 'Invalid information or missing attributes to create the task' });
+      return;
+    }
+  
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+});
 
-            tasks.splice(taskIndex, 1);
-             res.json({ message: 'Task deleted successfully' });
-             } else {
-             res.status(404).json({ message: 'Task not found' });
+router.delete('/delete/:id', (req, res) => {
+    const taskId = req.params.id;
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
+      res.status(204).json({ message: 'Task deleted successfully' }); // 204 No Content
+    } else {
+      res.status(404).json({ message: 'Task not found' });
     }
   });
 
-         router.put('/update/:id', (req, res) => {
-            const taskId = req.params.id;
-         const updatedTask = req.body; // Se espera un objeto con detalles actualizados de la tarea
-         const taskIndex = tasks.findIndex(task => task.id === taskId);
-            if (taskIndex !== -1) {
-            tasks[taskIndex] = updatedTask;
-             res.json(updatedTask);
-             } else {
-             res.status(404).json({ message: 'Task not found' });
+  router.put('/update/:id', (req, res) => {
+    const taskId = req.params.id;
+    const updatedTask = req.body; 
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    if (taskIndex !== -1) {
+      tasks[taskIndex] = updatedTask;
+      res.json(updatedTask);
+    } else {
+      res.status(404).json({ message: 'Task not found' });
     }
   });
 
-         return router;
+  return router;
 };
 
--- src/server/App.js
+++ src/server/App.js
@@ -1,24 +1,46 @@
 const express = require('express');
+const app = express();
 const port = 3000;
-require('dotenv').config();
 
+require('dotenv').config(); 
+
+// Importar las rutas y otros módulos
 const route = require('./router');
 const listEditRouter = require('./list-edit-router');
 const listViewRouter = require('./list-view-router');
+const showRouter = require('./show-router');
+const createTaskController = require('./create-task-controller'); 
+const updateTaskController = require('./update-task-controller'); 
+const deleteTaskController = require('./delete-task-controller'); 
+const listAllTasksController = require('./list-all-tasks-controller'); 
+const listCompletedTasksController = require('./list-completed-tasks-controller'); 
+const listIncompleteTasksController = require('./list-incomplete-tasks-controller'); 
+const getTaskByIdController = require('./get-task-by-id-controller'); 
 
 app.use(express.json());
 
 app.use('/api', route);
-app.use('/lists', listViewRouter);
-app.use('/edit', listEditRouter);
+app.use('/listas', listViewRouter);
+app.use('/editar', listEditRouter);
+app.use('/tasks', showRouter);
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the Task Management System');
-  });
+  res.status(200).send('Bienvenido Al Sistema De Gestión De Tareas');
+});
 
-  app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
-  });
+app.post('/api/tasks', createTaskController);
+app.put('/api/tasks/:id', updateTaskController);
+app.delete('/api/tasks/:id', deleteTaskController);
 
-  
-  
+app.get('/api/tasks', listAllTasksController);
+app.get('/api/tasks/complete', listCompletedTasksController);
+
+app.get('/api/tasks/incomplete', listIncompleteTasksController);
+
+app.get('/api/tasks/:id', getTaskByIdController);
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
+
+
